2d02abf324720519aa494dd49e56945929a5951a
manisha-13-code <manishachauhan5469@gmail.com>
2026-01-20 13:42:43 +0530

    commit

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,9 @@ app = FastAPI(title="Autonomous AI Project Manager")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://autonomous-project-manager-2.onrender.com/",
+        "http://localhost:3000",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
